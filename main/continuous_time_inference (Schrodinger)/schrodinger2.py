4e782eb42c311bfc8374881de4f4a30d2f344dd7
prantaneel-ti <120361228+prantaneel-ti@users.noreply.github.com>
2023-10-01 02:27:18 +0530

    fixed-plotting

--- main/continuous_time_inference (Schrodinger)/schrodinger2.py
+++ main/continuous_time_inference (Schrodinger)/schrodinger2.py
@@ -9,9 +9,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io
 from scipy.interpolate import griddata
-
+import matplotlib.gridspec as gridspec
 from pyDOE import lhs
 import time
+from plotting import newfig
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
 
 np.random.seed(43)
 tf.random.set_seed(43)
@@ -199,12 +202,14 @@ class PhysicsInformedNeuralNetwork:
                 print(f"Iteration {it + 1}, Loss : {self.loss.numpy()}")
 
     def predict(self, X_star):
+        X_star = tf.constant(X_star, dtype=tf.float32)
         x_pred = X_star[:, 0:1]
         t_pred = X_star[:, 1:2]
 
         u_pred, v_pred, _, _ = self.net_uv(x_pred, t_pred)
+        f_u_pred, f_v_pred = self.net_f_uv(x_pred, t_pred)
 
-        return u_pred, v_pred
+        return u_pred.numpy(), v_pred.numpy(), f_u_pred.numpy(), f_v_pred.numpy()
 
 
 """
@@ -273,9 +278,93 @@ if __name__ == "__main__":
     model = PhysicsInformedNeuralNetwork(x0, u0, v0, tb, X_f, layers, lb, ub)
 
     start_time = time.time()
-    model.train(50000)
+    model.train(10000)
     elapsed = time.time() - start_time
     print("Training time: %.4f" % (elapsed))
-
     u_pred, v_pred, f_u_pred, f_v_pred = model.predict(X_star)
     h_pred = np.sqrt(u_pred**2 + v_pred**2)
+    error_u = np.linalg.norm(u_star-u_pred,2)/np.linalg.norm(u_star,2)
+    error_v = np.linalg.norm(v_star-v_pred,2)/np.linalg.norm(v_star,2)
+    error_h = np.linalg.norm(h_star-h_pred,2)/np.linalg.norm(h_star,2)
+    print('Error u: %e' % (error_u))
+    print('Error v: %e' % (error_v))
+    print('Error h: %e' % (error_h))
+    
+    U_pred = griddata(X_star, u_pred.flatten(), (X, T), method='cubic')
+    V_pred = griddata(X_star, v_pred.flatten(), (X, T), method='cubic')
+    H_pred = griddata(X_star, h_pred.flatten(), (X, T), method='cubic')
+
+    FU_pred = griddata(X_star, f_u_pred.flatten(), (X, T), method='cubic')
+    FV_pred = griddata(X_star, f_v_pred.flatten(), (X, T), method='cubic')     
+    ######################################################################
+    ############################# Plotting ###############################
+    ######################################################################    
+    
+    X0 = np.concatenate((x0, 0*x0), 1) # (x0, 0)
+    X_lb = np.concatenate((0*tb + lb[0], tb), 1) # (lb[0], tb)
+    X_ub = np.concatenate((0*tb + ub[0], tb), 1) # (ub[0], tb)
+    X_u_train = np.vstack([X0, X_lb, X_ub])
+
+    fig, ax = newfig(1.0, 0.9)
+    ax.axis('off')
+    
+    ####### Row 0: h(t,x) ##################    
+    gs0 = gridspec.GridSpec(1, 2)
+    gs0.update(top=1-0.06, bottom=1-1/3, left=0.15, right=0.85, wspace=0)
+    ax = plt.subplot(gs0[:, :])
+    
+    h = ax.imshow(H_pred.T, interpolation='nearest', cmap='YlGnBu', 
+                  extent=[lb[1], ub[1], lb[0], ub[0]], 
+                  origin='lower', aspect='auto')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    fig.colorbar(h, cax=cax)
+    
+    ax.plot(X_u_train[:,1], X_u_train[:,0], 'kx', label = 'Data (%d points)' % (X_u_train.shape[0]), markersize = 4, clip_on = False)
+    
+    line = np.linspace(x.min(), x.max(), 2)[:,None]
+    ax.plot(t[75]*np.ones((2,1)), line, 'k--', linewidth = 1)
+    ax.plot(t[100]*np.ones((2,1)), line, 'k--', linewidth = 1)
+    ax.plot(t[125]*np.ones((2,1)), line, 'k--', linewidth = 1)    
+    
+    ax.set_xlabel('$t$')
+    ax.set_ylabel('$x$')
+    leg = ax.legend(frameon=False, loc = 'best')
+#    plt.setp(leg.get_texts(), color='w')
+    ax.set_title('$|h(t,x)|$', fontsize = 10)
+    
+    ####### Row 1: h(t,x) slices ##################    
+    gs1 = gridspec.GridSpec(1, 3)
+    gs1.update(top=1-1/3, bottom=0, left=0.1, right=0.9, wspace=0.5)
+    
+    ax = plt.subplot(gs1[0, 0])
+    ax.plot(x,Exact_h[:,75], 'b-', linewidth = 2, label = 'Exact')       
+    ax.plot(x,H_pred[75,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$|h(t,x)|$')    
+    ax.set_title('$t = %.2f$' % (t[75]), fontsize = 10)
+    ax.axis('square')
+    ax.set_xlim([-5.1,5.1])
+    ax.set_ylim([-0.1,5.1])
+    
+    ax = plt.subplot(gs1[0, 1])
+    ax.plot(x,Exact_h[:,100], 'b-', linewidth = 2, label = 'Exact')       
+    ax.plot(x,H_pred[100,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$|h(t,x)|$')
+    ax.axis('square')
+    ax.set_xlim([-5.1,5.1])
+    ax.set_ylim([-0.1,5.1])
+    ax.set_title('$t = %.2f$' % (t[100]), fontsize = 10)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.8), ncol=5, frameon=False)
+    
+    ax = plt.subplot(gs1[0, 2])
+    ax.plot(x,Exact_h[:,125], 'b-', linewidth = 2, label = 'Exact')       
+    ax.plot(x,H_pred[125,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$|h(t,x)|$')
+    ax.axis('square')
+    ax.set_xlim([-5.1,5.1])
+    ax.set_ylim([-0.1,5.1])    
+    ax.set_title('$t = %.2f$' % (t[125]), fontsize = 10)
+    plt.show()
